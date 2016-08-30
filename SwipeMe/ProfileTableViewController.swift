ce9cf3e51b093e34648883f9f9e1a9ce02b6595c
Tanaya Asnani <tasnani@umass.edugit config --global user.name Tanaya Asnani>
2016-08-29 23:02:43 -0400

    Added functionality to notification, logout, report a bug and invite a friend to new settings.

--- SwipeMe/ProfileTableViewController.swift
+++ SwipeMe/ProfileTableViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import FirebaseAuth
+import JSSAlertView
+import FirebaseInvites
+import GoogleSignIn
+import MessageUI
 
-class ProfileTableViewController: UITableViewController {
+
+class ProfileTableViewController: UITableViewController,FIRInviteDelegate,GIDSignInUIDelegate,MFMailComposeViewControllerDelegate  {
     
     var sectionsArray = [3,4,1,1]
 
+    @IBOutlet weak var notificationSwitch: UISwitch!
     
     
     override func viewDidLoad() {
@@ -24,6 +31,13 @@ class ProfileTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
+    @IBAction func switchPressed(sender: AnyObject) {
+        if(notificationSwitch.enabled == true ){
+            UIApplication.sharedApplication().registerForRemoteNotifications()
+        } else if(notificationSwitch.enabled == false){
+            UIApplication.sharedApplication().unregisterForRemoteNotifications()
+        }
+    }
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         print("Section : \(indexPath.section)")
         print("Index \(indexPath.row)")
@@ -32,6 +46,18 @@ class ProfileTableViewController: UITableViewController {
             
             if(indexPath.row == 1){
                 print("Invite Friend tapped")
+                if let invite = FIRInvites.inviteDialog() {
+                    invite.setInviteDelegate(self)
+                    
+                    
+                    invite.setMessage("Check this app out! It's called Swpr")
+                    invite.setTitle("Swpr invite! ")
+                    invite.setDeepLink("https://cq83n.app.goo.gl/qbvQ")
+                    invite.setCallToActionText("Install!")
+                    invite.setCustomImage("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+                    invite.open()
+                }
+                
             }
         }
         
@@ -58,13 +84,71 @@ class ProfileTableViewController: UITableViewController {
             
             if(indexPath.row == 0){
                 print("Report a Bug Tapped")
+                
+                let mailComposeViewController = configuredMailComposeViewController()
+                if MFMailComposeViewController.canSendMail() {
+                    self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+                } else {
+                    displayAlert("Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.")
+                }
             }
         }
         
         if(indexPath.section == 3){
             if(indexPath.row == 0){
                 print("Log Out Tapped")
+                try! FIRAuth.auth()!.signOut()
+                
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LogInScreen") as UIViewController
+                
+                self.presentViewController(viewController, animated: true, completion: nil)
             }
+        }
+    }
+    
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["info.swipeme@gmail.com"])
+        mailComposerVC.setSubject("Swpr Report A Bug ")
+        mailComposerVC.setMessageBody("", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func displayAlert(title:String, message: String){
+        let alertView = JSSAlertView().show(
+            self,
+            title: title,
+            text: message,
+            buttonText: "Okay",
+            color: UIColor(red:0.91, green:0.30, blue:0.24, alpha:1.0),
+            iconImage: UIImage(named: "idea"))
+        alertView.setTextTheme(.Light)
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    func displayNoInternetAlert(){
+        let alertView = JSSAlertView().show(
+            self,
+            title: "Oh 💩...",
+            text: "Looks like there is no internet. Connect to a network and relauch the app.",
+            buttonText: "FML😫 Okay",
+            color: UIColor(red:0.91, green:0.30, blue:0.24, alpha:1.0),
+            iconImage: UIImage(named: "noInternet"))
+        alertView.setTextTheme(.Light)
+    }
+    
+    func inviteFinishedWithInvitations(invitationIds: [AnyObject], error: NSError?) {
+        if let error = error {
+            print("Failed: " + error.localizedDescription)
+        } else {
+            print("Invitations sent")
         }
     }
 
